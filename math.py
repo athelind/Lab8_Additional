9a9bd362fa740437413220bd367960e517fe2f5d
athelind <edge.of.time@gmail.com>
2021-08-04 08:25:31 +0800

    Initial commit

---
+++ math.py
@@ -0,0 +1,16 @@
+class Operation:
+    def add(num1, num2):
+        result = num1 + num2
+        return result
+
+    def difference(num1, num2):
+        result = num1 - num2
+        return result
+
+    def product(num1, num2):
+        result = num1 * num2
+        return result
+
+    def division(num1, num2):
+        total = num1 / num2
+        return result
---
+++ testMath.py
@@ -0,0 +1,17 @@
+import unittest
+from math import Operation
+
+class testMath(unittest.TestCase):
+
+    def test_add(self):
+        result = Operation.add(1, 2)
+        self.assertEqual(result, 4)
+
+    def test_minus(self):
+        pass
+
+    def test_divide(self):
+        pass
+
+    def test_product(self):
+        pass
